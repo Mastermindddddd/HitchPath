df391f70da07405a5eec6eec2d5cd6cdfacdae52
Mastermindddddd <malusicephas17@gmail.com>
2024-12-27 18:36:44 +0200

    ...

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.use(limiter);
 
 // MongoDB connection
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
