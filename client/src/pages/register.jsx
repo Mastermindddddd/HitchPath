df3072d8e41e7c9d205a1c18b9dd2b5465ce6f5e
Mastermindddddd <malusicephas17@gmail.com>
2024-12-29 01:00:24 +0200

    ..

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -182,6 +182,15 @@ const Register = () => {
                 {loading ? <CircularProgress size={24} color="inherit" /> : "Register"}
               </button>
             </form>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              Already have an account?{" "}
+              <a
+                href="/login"
+                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              >
+                Login here
+              </a>
+            </p>
           </div>
         </div>
       </div>
